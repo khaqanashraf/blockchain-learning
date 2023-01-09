613e0ae6cb75cda5d48a3abf3357e2affe48e01e
Khaqan <khaqan.ashraf@hotwirestudios.io>
2023-01-09 14:56:10 +0500

    bsctestnet hardhat config setup. bscscan api key also setup in env to verify contracts

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,17 +1,22 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 require('dotenv').config()
 require( "@nomicfoundation/hardhat-toolbox")
+require("@nomiclabs/hardhat-etherscan")
 
 const RPC_URL = process.env.RPC_URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 
 module.exports = {
   solidity: "0.8.17",
   networks: {
-    bsc: {
+    bscTestnet: {
       url: RPC_URL,
       accounts: [PRIVATE_KEY],
-      chain: 31337
+      chainId: 97
     }
-  }
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY
+  },
 };
