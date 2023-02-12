62e96c59b47a00144c89cf2649805187b5bf8abe
Khaqan <khaqan.ashraf@hotwirestudios.io>
2023-02-12 08:27:37 +0500

    sort array in asc and desc order using insertion sort algo

--- test/dev/insertion_sort.test.js
+++ test/dev/insertion_sort.test.js
@@ -2,7 +2,7 @@ const { assert } = require('chai')
 const { network } = require('hardhat')
 const hre = require('hardhat')
 
-const CONTRACT_NAME = 'BubbleSort'
+const CONTRACT_NAME = 'InsertionSort'
 
 if (network.name === 'localhost')
     describe(`Testing ${CONTRACT_NAME} smart contract`, async () => {
@@ -17,29 +17,29 @@ if (network.name === 'localhost')
         })
 
         it('Should sort array in ascending order', async () => {
-            const array = [7, 3, 4, 2, 8, 5, 1, 9, 0, 6]
-            const sortedArray = await contract.sortAscending(array)
+            const arr = [7, 3, 4, 2, 8, 5, 1, 9, 0, 6]
+            const sortedArray = await contract.sortAsc(arr);
             let isSorted = true
-            for (let i = 0; i < array.length; i++) {
+            for (let i = 0; i < sortedArray.length; i++) {
                 const num = Number(sortedArray[i])
                 if (num !== i) {
-                    isSorted = false;
-                    break;
+                    isSorted = false
+                    break
                 }
             }
             assert(isSorted, 'Could not sort array in ascending order')
         })
 
         it('Should sort array in descending order', async () => {
-            const array = [7, 3, 4, 2, 8, 5, 1, 9, 0, 6]
-            const sortedArray = await contract.sortDescending(array)
+            const arr = [7, 3, 4, 2, 8, 5, 1, 9, 0, 6]
+            const sortedArray = await contract.sortDesc(arr);
             let isSorted = true
-            let n = array.length
+            let n = arr.length
             for (let i = 0; i < n; i++) {
                 const num = Number(sortedArray[i])
                 if (num !== n - 1 - i) {
-                    isSorted = false;
-                    break;
+                    isSorted = false
+                    break
                 }
             }
             assert(isSorted, 'Could not sort array in descending order')
