6b2c1dc4f3ffd47a975641dc9f23b88db63e9531
Khaqan <khaqan_ashraf@yahoo.com>
2023-02-24 14:31:01 +0500

    inheritence in solidity

---
+++ contracts/Inheritence.sol
@@ -0,0 +1,68 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+contract Person {
+    string s_name;
+
+    constructor(string memory _name) {
+        s_name = _name;
+    }
+
+    function setName(string calldata _name) public {
+        s_name = _name;
+    }
+
+    function getName() public view returns (string memory) {
+        return s_name;
+    }
+}
+
+contract Officer is Person {
+    uint s_stars;
+
+    constructor(uint _stars, string memory _name) Person(_name) {
+        s_stars = _stars;
+    }
+
+    function setStars(uint _stars) public {
+        s_stars = _stars;
+    }
+
+    function getStars() public view returns (uint) {
+        return s_stars;
+    }
+}
+
+contract Cop is Officer {
+    uint s_rank;
+
+    constructor(
+        uint _rank,
+        uint _stars,
+        string memory _name
+    ) Officer(_stars, _name) {
+        s_rank = _rank;
+    }
+
+    function setRank(uint _rank) public {
+        s_rank = _rank;
+    }
+
+    function getRank() public view returns (uint) {
+        return s_rank;
+    }
+
+    function getAttributes()
+        public
+        view
+        returns (
+            uint rank,
+            uint stars,
+            string memory name
+        )
+    {
+        rank = this.getRank();
+        stars = this.getStars();
+        name = this.getName();
+    }
+}
