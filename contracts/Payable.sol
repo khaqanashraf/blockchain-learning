fe2f9f4281d7f74cc0158231725f301189e68f76
Khaqan <khaqan_ashraf@yahoo.com>
2023-03-04 20:25:47 +0500

    unit test for payable contract to get and transfer ethers

--- contracts/Payable.sol
+++ contracts/Payable.sol
@@ -4,15 +4,28 @@ pragma solidity ^0.8.17;
 contract Funds {
     address payable immutable i_owner;
 
+    modifier isOwner() {
+        require(i_owner == msg.sender, "Not Owner");
+        _;
+    }
+
     constructor() {
         i_owner = payable(msg.sender);
+    }
+
+    function amIOwner() public view returns (bool) {
+        return i_owner == msg.sender;
+    }
+
+    function getBalance() public view returns (uint) {
+        return address(this).balance;
     }
 
     function deposite() public payable {}
 
     function depositeNonPayable() public {}
 
-    function withdraw() public {
+    function withdraw() public isOwner {
         uint amount = address(this).balance;
         (bool _success, ) = i_owner.call{value: amount}("");
         require(_success, "Failed to withdraw");
