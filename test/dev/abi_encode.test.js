5d231446a52a083c792b27348dd0d9b5802b98ca
Khaqan <khaqan_ashraf@yahoo.com>
2023-03-17 14:08:39 +0500

    unit test for abiencode

--- test/dev/abi_encode.test.js
+++ test/dev/abi_encode.test.js
@@ -18,5 +18,29 @@ if (network.name === 'localhost')
             assert(abiEncode.address && token.address, 'Contract is not deployed successfuly!')
         })
 
+        it('Should get encoded with signature', async () => {
+            const signers = await hre.ethers.getSigners()
+            const signature = await abiEncode.encodeWithSignature(signers[1].address, String(0.1 * 1e18))
+            assert(signature, 'Could not get signature')
+        })
+
+        it('Should get encoded with selector', async () => {
+            const signers = await hre.ethers.getSigners()
+            const signature = await abiEncode.encodeWithSelector(signers[1].address, String(0.1 * 1e18))
+            assert(signature, 'Could not get signature')
+        })
+
+        it('Should get encoded call', async () => {
+            const signers = await hre.ethers.getSigners()
+            const signature = await abiEncode.encodeCall(signers[1].address, String(0.1 * 1e18))
+            assert(signature, 'Could not get signature')
+        })
+
+        it('Should call transfer function with encoded signature', async () => {
+            const signers = await hre.ethers.getSigners()
+            const data = await abiEncode.encodeWithSignature(signers[1].address, String(0.1 * 1e18))
+            await abiEncode.test(token.address, data)
+            assert(true, 'Call failed with abi encoded data')
+        })
 
     })
