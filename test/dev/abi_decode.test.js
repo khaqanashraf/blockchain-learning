fa319ba56c49cd781031ebca5d4c9f6b4d86f16b
Khaqan <khaqan_ashraf@yahoo.com>
2023-03-18 16:10:32 +0500

    two unit tests for AbiDecode contract

--- test/dev/abi_decode.test.js
+++ test/dev/abi_decode.test.js
@@ -15,6 +15,18 @@ if (network.name === 'localhost')
             assert(contract.address, 'Contract is not deployed successfuly!')
         })
 
+        let encoded
+        it('Should encode data successfully', async () => {
+            const signers = await hre.ethers.getSigners()
+            encoded = await contract.encode(signers[1].address, String(0.1 * 1e18))
+            assert(encoded, 'Could not encode data successfully')
+        })
+
+        it('Should decode data successfully', async () => {
+            const signers = await hre.ethers.getSigners()
+            const decoded = await contract.decode(encoded)
+            assert(decoded[0] == signers[1].address && decoded[1] == String(0.1 * 1e18), 'Could not decode data successfully')
+        })
 
 
     })
