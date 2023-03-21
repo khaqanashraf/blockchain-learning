9db9059cdfb6997f3fe41b5732a7dab76db882ec
Khaqan <khaqan_ashraf@yahoo.com>
2023-03-21 22:48:19 +0500

    Bitwise.sol indentation

--- contracts/Bitwise.sol
+++ contracts/Bitwise.sol
@@ -2,35 +2,33 @@
 pragma solidity ^0.8.17;
 
 contract BitwiseOps {
-    constructor() {
-        
-    }
+    constructor() {}
 
-    function and(uint x, uint y) public pure returns(uint){
+    function and(uint x, uint y) public pure returns (uint) {
         return x & y;
     }
 
-    function or(uint x, uint y) public pure returns(uint){
+    function or(uint x, uint y) public pure returns (uint) {
         return x | y;
     }
 
-    function xor(uint x, uint y) public pure returns(uint){
+    function xor(uint x, uint y) public pure returns (uint) {
         return x ^ y;
     }
 
-    function not(uint x) public pure returns(uint){
+    function not(uint x) public pure returns (uint) {
         return ~x;
     }
 
-    function shiftLef(uint x, uint bits) public pure returns(uint){
+    function shiftLef(uint x, uint bits) public pure returns (uint) {
         return x << bits;
     }
 
-    function shiftRight(uint x, uint bits) public pure returns(uint){
+    function shiftRight(uint x, uint bits) public pure returns (uint) {
         return x >> bits;
     }
 
-    function getMask(uint bits) public pure returns(uint){
+    function getMask(uint bits) public pure returns (uint) {
         return (1 << bits) - 1;
     }
 }
