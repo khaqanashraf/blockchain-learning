aecbbcf0f2db088aec47a67d365bf15af48f09f7
Khaqan <khaqan_ashraf@yahoo.com>
2023-03-29 15:15:44 +0500

    test case to check owner of an NFT in BasicNFT

--- test/dev/basicNFT.test.js
+++ test/dev/basicNFT.test.js
@@ -7,7 +7,9 @@ const CONTRACT_NAME = 'BasicNFT'
 if (network.name === 'localhost')
     describe(`Testing ${CONTRACT_NAME} smart contract`, async () => {
         let contract
+        let accounts
         before(async () => {
+            accounts = await hre.ethers.getSigners()
             const Contract = await hre.ethers.getContractFactory(CONTRACT_NAME)
             contract = await Contract.deploy()
         })
@@ -22,7 +24,7 @@ if (network.name === 'localhost')
             assert(tokenId === 0, 'Could not mint an nft and get 0 tokenid')
         })
         it('Should mint a new nft and should have tokenId 1', async () => {
-            await contract.mint()
+            await contract.connect(accounts[1]).mint()
             const tokenId = Number(await contract.getTokenCounter()) - 1
             assert(tokenId === 1, 'Could not mint an nft and get 1 tokenid')
         })
@@ -40,6 +42,11 @@ if (network.name === 'localhost')
             const tokenUri0 = await contract.tokenURI(0)
             const tokenUri3 = await contract.tokenURI(3)
             assert(tokenUri0 === tokenUri3, 'Could not get same token uri for token id 0 and 3')
+        })
+
+        it('Should get owner of token id 1 to be account[1]', async () => {
+            const owner = await contract.ownerOf(1)
+            assert(owner === accounts[1].address, 'Could not get the correct owner of token id 1')
         })
 
     })
